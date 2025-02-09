b8f04871888a65834c2d683dcf76959680020259
Vaishno Chaitanya <17123227+vchan-in@users.noreply.github.com>
2025-02-09 06:48:04 +0000

    remove: delete TestHandleCORS function from handlers_test.go

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -68,28 +68,6 @@ func TestGetRoot(t *testing.T) {
 	}
 }
 
-func TestHandleCORS(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodOptions, "/", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-
-	handler := HandleCORS(func(c echo.Context) error {
-		return c.String(http.StatusOK, "test")
-	})
-
-	os.Setenv("DM_CORS_ORIGIN", "*")
-	defer os.Unsetenv("DM_CORS_ORIGIN")
-
-	if assert.NoError(t, handler(c)) {
-		assert.Equal(t, "*", rec.Header().Get("Access-Control-Allow-Origin"))
-		assert.Equal(t, "GET", rec.Header().Get("Access-Control-Allow-Methods"))
-		assert.Equal(t, "Content-Type, Authorization", rec.Header().Get("Access-Control-Allow-Headers"))
-		assert.Equal(t, "Content-Length, Content-Range", rec.Header().Get("Access-Control-Expose-Headers"))
-		assert.Equal(t, "true", rec.Header().Get("Access-Control-Allow-Credentials"))
-	}
-}
-
 func TestHandleAuthMiddleware(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
